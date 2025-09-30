7973584542cab93817c308ce45f0090fe2e56fa8
Rahul T George <34202955+rahul-george@users.noreply.github.com>
2025-09-30 13:18:37 +0000

    feat: add gradient to mouse hover

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-function createNode(flex_basis) {
+let SELECTED_COLOR = "#25242b";
+
+function createNode(flex_basis, bgColor) {
   const originalDiv = document.createElement("div");
-  originalDiv.style = `flex-basis: ${flex_basis}%`; // Other styles are injected via CSS
+  originalDiv.style = `flex-basis: ${flex_basis}%; 
+  background-color: ${bgColor}; 
+  opacity: 0`; // Other styles are injected via CSS
   originalDiv.addEventListener("mouseenter", paintDivBlack);
   return originalDiv;
 }
 
-function generateDivs(size = 16) {
+function generateDivs(size = 16, bgColor) {
   const flex_basis = 100 / size;
   const documentFragment = document.createDocumentFragment();
   for (let i = 0; i < size * size; i++) {
-    const gridCell = createNode(flex_basis);
+    const gridCell = createNode(flex_basis, bgColor);
     documentFragment.appendChild(gridCell);
   }
   container.appendChild(documentFragment);
 }
 
 function paintDivBlack(e) {
-  // console.log({ e });
   const target = e.target;
-  target.classList.add("cell");
+  target.style.opacity = parseFloat(target.style.opacity) + 0.1;
   e.stopPropagation();
 }
 
 const container = document.querySelector(".container");
 
-generateDivs(16);
+generateDivs(16, SELECTED_COLOR);
