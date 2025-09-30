70e0a45a847b354afa7a355d1da27ff0cd9c3e48
Rahul T George <34202955+rahul-george@users.noreply.github.com>
2025-09-30 10:25:18 +0000

    refactor: reverted premature optimization, added event handler

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 function createNode(flex_basis) {
   const originalDiv = document.createElement("div");
   originalDiv.style = `flex-basis: ${flex_basis}%`; // Other styles are injected via CSS
+  originalDiv.addEventListener("click", paintDivBlack);
+  return originalDiv;
 }
 
 function generateDivs(size = 4) {
   const flex_basis = 100 / size;
-  const originalDiv = createNode(flex_basis);
-  const documentFragment = document.createDocumentFragment();
-
   for (let i = 0; i < size * size; i++) {
-    documentFragment.appendChild(originalDiv.cloneNode());
+    container.appendChild(createNode(flex_basis));
   }
-  container.appendChild(documentFragment);
+}
+
+function paintDivBlack(e) {
+  console.log({ e });
 }
 
 const container = document.querySelector(".container");
