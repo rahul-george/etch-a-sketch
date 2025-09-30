d22b5ae14a12dd45dda2b87b4a6b3c9f7478e3a0
Rahul T George <34202955+rahul-george@users.noreply.github.com>
2025-09-30 12:58:18 +0000

    feat: color cell on hover

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 function createNode(flex_basis) {
   const originalDiv = document.createElement("div");
   originalDiv.style = `flex-basis: ${flex_basis}%`; // Other styles are injected via CSS
-  originalDiv.addEventListener("click", paintDivBlack);
+  originalDiv.addEventListener("mouseenter", paintDivBlack);
   return originalDiv;
 }
 
-function generateDivs(size = 4) {
+function generateDivs(size = 16) {
   const flex_basis = 100 / size;
+  const documentFragment = document.createDocumentFragment();
   for (let i = 0; i < size * size; i++) {
-    container.appendChild(createNode(flex_basis));
+    const gridCell = createNode(flex_basis);
+    documentFragment.appendChild(gridCell);
   }
+  container.appendChild(documentFragment);
 }
 
 function paintDivBlack(e) {
-  console.log({ e });
+  // console.log({ e });
+  const target = e.target;
+  target.classList.add("cell");
+  e.stopPropagation();
 }
 
 const container = document.querySelector(".container");
 
-generateDivs(10);
+generateDivs(16);
