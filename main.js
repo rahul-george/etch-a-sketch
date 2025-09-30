567a8f3065f73e77c081dd6f330d41594277f9fc
Rahul T George <34202955+rahul-george@users.noreply.github.com>
2025-09-30 10:13:02 +0000

    refactor: Used documentFragment and cloneElement. Could be premature optimization

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
+function createNode(flex_basis) {
+  const originalDiv = document.createElement("div");
+  originalDiv.style = `flex-basis: ${flex_basis}%`; // Other styles are injected via CSS
+}
+
 function generateDivs(size = 4) {
   const flex_basis = 100 / size;
+  const originalDiv = createNode(flex_basis);
+  const documentFragment = document.createDocumentFragment();
+
   for (let i = 0; i < size * size; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.style = `flex-basis: ${flex_basis}%`; // Other styles are injected via CSS
-    container.appendChild(newDiv);
+    documentFragment.appendChild(originalDiv.cloneNode());
   }
+  container.appendChild(documentFragment);
 }
 
 const container = document.querySelector(".container");
 
-generateDivs(12);
+generateDivs(10);
